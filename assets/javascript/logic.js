f9c8776ba399f921ec74a2c66c8f00d244880d92
Jennifer Faye <jennifer@fayecreative.com>
2021-03-02 22:56:58 -0600

    pulling in one category from API

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,10 +1,32 @@
 $(document).ready()
 
 
+const queryURL = "https://www.themealdb.com/api/json/v1/1/categories.php";
 $.ajax({
-  url: "https://www.themealdb.com/api/json/v1/1/categories.php",
+  url:queryURL,
   method: "GET"
 }).then(function(response) {
   console.log(response);
-});
 
+  // Render to HTML
+  const food = response.categories[2];
+  const foodDiv= document.getElementById("foodView");
+
+  // Output the data - name
+  const foodName= food.strCategory;
+  const heading = document.createElement("h4");
+  heading.innerHTML = foodName;
+  foodDiv.appendChild(heading);
+
+  // Outout the data - image
+  const foodImage = document.createElement("img");
+  foodImage.src = food.strCategoryThumb;
+  foodDiv.appendChild(foodImage);
+
+  //Output the data - description
+  const foodDescrip = food.strCategoryDescription;
+  const info = document.createElement("h6");
+  info.innerHTML = foodDescrip;
+  foodDiv.appendChild(info);
+
+})
