cf02fe31f797e2347b70991502eb731ba64d23dc
Jennifer Faye <jennifer@fayecreative.com>
2021-06-05 11:27:16 -0500

    pulling one ingredient and measure from db

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -3,36 +3,60 @@ $(document).ready(function(){
   $('.sidenav').sidenav(); 
 });
 
-(function clickMe() {
-  const button = document.getElementById("button");
-  button.addEventListener("click", event => {
-   // Pulling data from TheMealDB API
+$(document).ready(function() {
+  $('#button').click(connectDb);
+});
+
+function connectDb() {
+   //Pulling data from TheMealDB API
   const queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
   $.ajax({
     url:queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
+  renderDrinks(response)
+  })
+};
 
-    // Render to HTML
-    const drink = response.drinks[0];
-    const drinkDiv= document.getElementById("cocktailView");
+function renderDrinks(response) {
+  // Render to HTML
+    let drink = response.drinks[0];
+    let drinkDiv = document.getElementById("cocktailView");
 
     // Output the data - name
-    const drinkName= drink.strDrink;
-    const heading = document.createElement("h3");
+    let drinkName = drink.strDrink;
+    let heading = document.createElement("h3");
     heading.innerHTML = drinkName;
     drinkDiv.appendChild(heading);
 
     // Outout the data - image
-    const drinkImage = document.createElement("img");
+    let drinkImage = document.createElement("img");
     drinkImage.src = drink.strDrinkThumb;
     drinkDiv.appendChild(drinkImage);
 
-    //Output the data - ingredients - loop through and select only those without null
-  
-    })
-  });
-})();
+    
+    renderIngr(drink, drinkDiv);
+};
+
+function renderIngr (drink, drinkDiv) {
+  let drinkIngr = drink.strIngredient1;
+  let drinkMsr = drink.strMeasure1;
+  let ingrOne = document.createElement("li");
+  let msrOne = document.createElement("li")
+  ingrOne.innerHTML = drinkIngr;
+  msrOne.innerHTML = drinkMsr;
+  if( drinkIngr !== 'null' ) {
+    drinkDiv.appendChild(ingrOne);
+    drinkDiv.appendChild(msrOne);
+    }
+}
 
 
+
+//     //Output the data - ingredients - loop through and select only those without null
+//     const ingredients = drink.strIngredient2;
+//     console.log (ingredients);
+//     })
+//   });
+// })();
