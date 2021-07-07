f24fb6f19a6b2b75fe77bacc2efc4b261858f187
Jennifer Faye <jennifer@fayecreative.com>
2021-07-06 23:10:43 -0500

    cocktail image and ingrediants and measurements are now displayed

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -95,40 +95,45 @@ function renderIngrs (drink, drinkDiv){
   let drinkMsr15 = drink.strMeasure15;
 
   const allIngr = [
-    drinkMsr1 + "of " + drinkIngr1,
-    drinkIngr2,
+    drinkIngr1, 
+    drinkMsr1,
+    drinkIngr2, 
     drinkMsr2,
-    drinkIngr3,
+    drinkIngr3, 
     drinkMsr3,
-    drinkIngr4,
+    drinkIngr4, 
     drinkMsr4,
-    drinkIngr5,
+    drinkIngr5, 
     drinkMsr5,
-    drinkIngr6,
+    drinkIngr6, 
     drinkMsr6,
-    drinkIngr7,
+    drinkIngr7, 
     drinkMsr7,
-    drinkIngr8,
+    drinkIngr8, 
     drinkMsr8,
-    drinkIngr9,
+    drinkIngr9, 
     drinkMsr9,
-    drinkIngr10,
+    drinkIngr10, 
     drinkMsr10,
-    drinkIngr11,
+    drinkIngr11, 
     drinkMsr11,
-    drinkIngr12,
+    drinkIngr12, 
     drinkMsr12,
     drinkIngr13,
     drinkMsr13,
-    drinkIngr14,
+    drinkIngr14, 
     drinkMsr14,
     drinkIngr15,
-    drinkMsr15,
+    drinkMsr15
   ];
 
-  $.each(allIngr, function(index, value){
-    if(value !== null) 
-    console.log(value);
+  $.each(allIngr, function(index, value){ 
+    if (value !== null) {
+    } else {return false};
+    console.log(value) 
+    let item = document.createElement("li");
+    item.innerHTML=value;
+    drinkDiv.appendChild(item);
   });
 
   // let msrIngr1 = (drinkMsr1 + " " + drinkIngr1);
@@ -189,45 +194,47 @@ function renderIngrs (drink, drinkDiv){
 //     drinkDiv.appendChild(item4);
 //     } 
 
-//   let drinkIngr5 = drink.strIngredient5;
-//   let drinkMsr5 = drink.strMeasure5;
+  // let drinkIngr5 = drink.strIngredient5;
+  // let drinkMsr5 = drink.strMeasure5;
 
-//   if(drinkIngr5 !== 'null') {
-//     let msrIngr5 = (drinkMsr5 + " " + drinkIngr5);
-//     let item5 = document.createElement("li");
-//     item5.innerHTML = msrIngr5;
-//     drinkDiv.appendChild(item5);
-//     } 
+  // if(drinkIngr5 !== 'null') {
+  //   let msrIngr5 = (drinkMsr5 + " " + drinkIngr5);
+  //   let item5 = document.createElement("li");
+  //   item5.innerHTML = msrIngr5;
+  //   drinkDiv.appendChild(item5);
+  //   } else {
+      
+  //   }
 
-//   let drinkIngr6 = drink.strIngredient6;
-//   let drinkMsr6 = drink.strMeasure6;
+  // let drinkIngr6 = drink.strIngredient6;
+  // let drinkMsr6 = drink.strMeasure6;
 
-//   if(drinkIngr6 !== 'null') {
-//     let msrIngr6 = (drinkMsr6 + " " + drinkIngr6);
-//     let item6 = document.createElement("li");
-//     item6.innerHTML = msrIngr6;
-//     drinkDiv.appendChild(item6);
-//     } 
+  // if(drinkIngr6 !== 'null') {
+  //   let msrIngr6 = (drinkMsr6 + " " + drinkIngr6);
+  //   let item6 = document.createElement("li");
+  //   item6.innerHTML = msrIngr6;
+  //   drinkDiv.appendChild(item6);
+  //   } 
 
-//   let drinkIngr7 = drink.strIngredient7;
-//   let drinkMsr7 = drink.strMeasure7;
+  // let drinkIngr7 = drink.strIngredient7;
+  // let drinkMsr7 = drink.strMeasure7;
 
-//   if(drinkIngr7 !== 'null') {
-//     let msrIngr7 = (drinkMsr7 + " " + drinkIngr7);
-//     let item7 = document.createElement("li");
-//     item7.innerHTML = msrIngr7;
-//     drinkDiv.appendChild(item7);
-//     } 
+  // if(drinkIngr7 !== 'null') {
+  //   let msrIngr7 = (drinkMsr7 + " " + drinkIngr7);
+  //   let item7 = document.createElement("li");
+  //   item7.innerHTML = msrIngr7;
+  //   drinkDiv.appendChild(item7);
+  //   } 
 
-//   let drinkIngr8 = drink.strIngredient8;
-//   let drinkMsr8 = drink.strMeasure8;
+  // let drinkIngr8 = drink.strIngredient8;
+  // let drinkMsr8 = drink.strMeasure8;
 
-//   if(drinkIngr8 !== 'null') {
-//     let msrIngr8 = (drinkMsr8 + " " + drinkIngr8);
-//     let item8 = document.createElement("li");
-//     item8.innerHTML = msrIngr8;
-//     drinkDiv.appendChild(item8);
-//     } 
+  // if(drinkIngr8 !== 'null') {
+  //   let msrIngr8 = (drinkMsr8 + " " + drinkIngr8);
+  //   let item8 = document.createElement("li");
+  //   item8.innerHTML = msrIngr8;
+  //   drinkDiv.appendChild(item8);
+  //   } 
 
 //   let drinkIngr9 = drink.strIngredient9;
 //   let drinkMsr9 = drink.strMeasure9;
