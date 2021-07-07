4edaf9583693654cb1cb3d0d206308fe7ba13734
Jennifer Faye <jennifer@fayecreative.com>
2021-07-06 23:48:56 -0500

    ingredients, type of drink, glass, rendering to html

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -21,31 +21,45 @@ function connectDb() {
 
 function renderDrinks(response) {
   // Render to HTML
-    let drink = response.drinks[0];
-    let drinkDiv = document.getElementById("cocktailView");
+  let drink = response.drinks[0];
+  let drinkDiv = document.getElementById("cocktailView");
 
-    // Output the data - name
-    let drinkName = drink.strDrink;
-    let heading = document.createElement("h3");
-    heading.innerHTML = drinkName;
-    drinkDiv.appendChild(heading);
+  // Output the data - name
+  let drinkName = drink.strDrink;
+  let heading = document.createElement("h3");
+  heading.innerHTML = drinkName;
+  drinkDiv.appendChild(heading);
+
+  // Output the data - alcoholic/non
+  let drinkAlco = drink.strAlcoholic;
+
+   // Output the data - glass
+  let drinkGlass = drink.strGlass;
+
+  //Render alcoholic and glass to html 
+  let alcoGlass = ("This is an" + " " + drinkAlco + " " + "drink using a" + " " + drinkGlass + ".")
+  let glassAlco = document.createElement("p");
+  glassAlco.innerHTML = alcoGlass;
+  drinkDiv.appendChild(glassAlco);
+
 
     // Outout the data - image
     let drinkImage = document.createElement("img");
     drinkImage.src = drink.strDrinkThumb;
     drinkDiv.appendChild(drinkImage);
 
-    
-    renderIngrs(drink, drinkDiv);
+    renderDirections(drink, drinkDiv);
 };
 
-var caption;
+function renderDirections(drink, drinkDiv){
+  // Output the data - instructions
+  let drinkInstr = drink.strInstructions;
+  let howTo = document.createElement("p");
+  howTo.innerHTML = drinkInstr;
+  drinkDiv.appendChild(howTo);
 
-// if(photo.caption != null) { // Covers 'undefined' as well
-//   caption = photo.caption.text;
-// } else {
-//   caption = "meh";
-// }
+  renderIngrs(drink, drinkDiv);
+}
 
 function renderIngrs (drink, drinkDiv){
 
@@ -95,59 +109,50 @@ function renderIngrs (drink, drinkDiv){
   let drinkMsr15 = drink.strMeasure15;
 
   const allIngr = [
-    drinkIngr1, 
     drinkMsr1,
-    drinkIngr2, 
+    drinkIngr1, 
     drinkMsr2,
-    drinkIngr3, 
+    drinkIngr2, 
     drinkMsr3,
-    drinkIngr4, 
+    drinkIngr3, 
     drinkMsr4,
-    drinkIngr5, 
+    drinkIngr4, 
     drinkMsr5,
-    drinkIngr6, 
+    drinkIngr5, 
     drinkMsr6,
-    drinkIngr7, 
+    drinkIngr6, 
     drinkMsr7,
-    drinkIngr8, 
+    drinkIngr7, 
     drinkMsr8,
-    drinkIngr9, 
+    drinkIngr8, 
     drinkMsr9,
-    drinkIngr10, 
+    drinkIngr9, 
     drinkMsr10,
-    drinkIngr11, 
+    drinkIngr10, 
     drinkMsr11,
-    drinkIngr12, 
+    drinkIngr11, 
     drinkMsr12,
-    drinkIngr13,
+    drinkIngr12, 
     drinkMsr13,
-    drinkIngr14, 
+    drinkIngr13,
     drinkMsr14,
-    drinkIngr15,
-    drinkMsr15
+    drinkIngr14, 
+    drinkMsr15,
+    drinkIngr15
   ];
 
+  // Output the data - measurements, ingredients
   $.each(allIngr, function(index, value){ 
     if (value !== null) {
-    } else {return false};
+    } 
     console.log(value) 
-    let item = document.createElement("li");
+    let item = document.createElement("ol");
     item.innerHTML=value;
     drinkDiv.appendChild(item);
   });
-
-  // let msrIngr1 = (drinkMsr1 + " " + drinkIngr1);
-  // let item1 = document.createElement("li");
-  // item1.innerHTML = msrIngr1;
-  // drinkDiv.appendChild(item1);
-
-  // let msrIngr15 = (drinkMsr15 + " " + drinkIngr15);
-  // let item15 = document.createElement("li");
-  // item15.innerHTML = msrIngr15;
-  // drinkDiv.appendChild(item15);
-
-
 }
+
+
 
 
 
