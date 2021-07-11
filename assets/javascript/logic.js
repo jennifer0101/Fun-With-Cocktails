503dd372bf778cf77a5c4e639c7ca3e380cc3c57
Jennifer Faye <jennifer@fayecreative.com>
2021-07-11 00:14:53 -0500

    first button fills img and name, second button click fills ingr and instru

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
   $('.scrollspy').scrollSpy();
   $('.sidenav').sidenav(); 
-});
-
-$(document).ready(function() {
   $('#button').click(connectDb);
-});
 
 function connectDb() {
+  //Empty all the div fields of any previous cocktail data
+  $("#nameView").empty();
+  $("#cocktailView").empty();
+  $("#ingrView").empty();
+  $("#instruView").empty();
    //Pulling data from TheMealDB API
   const queryURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
   $.ajax({
@@ -22,13 +23,14 @@ function connectDb() {
 function renderDrinks(response) {
   // Render to HTML
   let drink = response.drinks[0];
+  let nameDiv = document.getElementById("nameView");
   let drinkDiv = document.getElementById("cocktailView");
 
   // Output the data - name
   let drinkName = drink.strDrink;
   let heading = document.createElement("h3");
   heading.innerHTML = drinkName;
-  drinkDiv.appendChild(heading);
+  nameDiv.appendChild(heading);
 
   // Pull the data - alcoholic/non
   let drinkAlco = drink.strAlcoholic;
@@ -40,23 +42,31 @@ function renderDrinks(response) {
   let alcoGlass = ("This is an" + " " + drinkAlco + " " + "drink using a" + " " + drinkGlass + ".")
   let glassAlco = document.createElement("h5");
   glassAlco.innerHTML = alcoGlass;
-  drinkDiv.appendChild(glassAlco);
+  nameDiv.appendChild(glassAlco);
 
   // Outout the data - image
   let drinkImage = document.createElement("img");
   drinkImage.src = drink.strDrinkThumb;
   drinkDiv.appendChild(drinkImage);
 
+  renderIngrs(drink);
+};
+
+function renderIngrs (drink){
+  $('#button2').click(function(){
+  $("#ingrView").empty();
+  $("#instruView").empty();
+  
+  //Render to HTML
+  let instruDiv = document.getElementById("instruView");
+  let ingrDiv = document.getElementById("ingrView");
+
   // Output the data - instructions
   let drinkInstr = drink.strInstructions;
   let howTo = document.createElement("p");
   howTo.innerHTML = drinkInstr;
-  drinkDiv.appendChild(howTo);
+  instruDiv.appendChild(howTo);
 
-  renderIngrs (drink, drinkDiv);
-};
-
-function renderIngrs (drink, drinkDiv){
   //Pull each ingredient and each measurment
   let drinkIngr1 = drink.strIngredient1;
   let drinkMsr1 = drink.strMeasure1;
@@ -144,6 +154,7 @@ function renderIngrs (drink, drinkDiv){
     console.log(value) 
     let item = document.createElement("ol");
     item.innerHTML=value;
-    drinkDiv.appendChild(item);
+    ingrDiv.appendChild(item);
   });
-}
+})
+}});
